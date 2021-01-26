f7d06ff41396d988790ea904a75de936d7267143
LubaEremenko <21314@student.dorset-college.ie>
2021-01-26 22:36:24 +0000

    commit

---
+++ lab.js
@@ -0,0 +1,27 @@
+var myAlphabet; 
+myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G']; 
+console.log("The length of myAlphabet is " + myAlphabet.length);
+
+var planets = ['Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn'];
+console.log(planets[4]);
+console.log(planets[3]);
+console.log(planets[2]);
+console.log(planets[1]);
+console.log(planets[0]);
+
+let wowDatatypes = ['John', 66, true ];
+console.log(wowDatatypes);
+
+function hello() {
+    return 'hello world';
+};
+var result = hello();
+console.log(hello());
+
+
+var hello = function() {
+    return 'hi';
+};
+var x = hello();
+console.log(x);
+
